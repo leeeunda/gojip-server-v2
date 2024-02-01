ed12a857f9b90e03314a3c3c79d8e75e92d2a22e
strongmhk <kim6562166086@gmail.com>
2024-02-02 00:48:20 +0900

    feat: CheckList 엔티티 설계

---
+++ src/main/java/com/example/gojipserver/domain/checklist/entity/CheckList.java
@@ -0,0 +1,51 @@
+package com.example.gojipserver.domain.checklist.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class CheckList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "checklist_id")
+    private Long id;
+
+    @OneToOne
+    private Address address;
+
+    private int deposit; //보증금
+
+    private int monthlyCost; //월세
+
+    private int managementCost; //관리비
+
+    @Embedded
+    private ManagementCostIncludeOption managementCostIncludeOption; //관리비 포함 옵션
+
+    @Embedded
+    private Status status;
+
+    @Embedded
+    private Noise noise; //소음
+
+    @Embedded
+    private RoomOption roomOption;
+
+    private int area; //평수
+
+    private int stationDistance; //역과의 거리
+
+    private String text; //이미지 설명
+
+    @ManyToOne(fetch = LAZY)
+    private Collection collection;
+
+
+}
---
+++ src/main/java/com/example/gojipserver/domain/checklist/entity/Collection.java
@@ -0,0 +1,17 @@
+package com.example.gojipserver.domain.checklist.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Collection {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}

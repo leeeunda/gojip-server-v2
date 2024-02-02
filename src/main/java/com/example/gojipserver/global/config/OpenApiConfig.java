f8c5cf89b75b05ff98ca5cd3c50b35a72fddeda3
SeoYoung <seoyoung7623@naver.com>
2024-02-02 23:41:09 +0900

    feat: swagger(openApi) 적용

---
+++ src/main/java/com/example/gojipserver/global/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.example.gojipserver.global.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("Gojip API 명세서")
+                .version("v0.0.1")
+                .description("Gojip API 명세서입니다.");
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+}

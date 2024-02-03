261679b9c82abd324156c18770d60cbf326a3275
strongmhk <kim6562166086@gmail.com>
2024-02-03 15:59:03 +0900

    feat: s3 이미지 다중 업로드, 단일 삭제 구현
    - 단일 삭제 예외처리(파일 이름을 찾을 수 없을 경우) 필요함

---
+++ src/main/java/com/example/gojipserver/global/aws/controller/AmazonS3Controller.java
@@ -0,0 +1,38 @@
+package com.example.gojipserver.global.aws.controller;
+
+import com.example.gojipserver.global.aws.service.AwsS3Service;
+import com.example.gojipserver.global.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/s3")
+public class AmazonS3Controller {
+
+    private final AwsS3Service awsS3Service;
+
+    /**
+     * Amazon S3에 파일 업로드
+     *
+     * @return 성공 시 200 Success와 함께 업로드 된 파일의 파일명 리스트 반환
+     */
+    @PostMapping("/files")
+    public ApiResponse<List<String>> uploadFile(@RequestPart List<MultipartFile> multipartFile) {
+        return ApiResponse.createSuccess(awsS3Service.uploadFile(multipartFile));
+    }
+
+    /**
+     * Amazon S3에 업로드 된 파일을 삭제
+     *
+     * @return 성공 시 200 Success
+     */
+    @DeleteMapping("/files")
+    public ApiResponse<Object> deleteFile(@RequestParam String fileName) {
+        awsS3Service.deleteFile(fileName);
+        return ApiResponse.createSuccess(null);
+    }
+}

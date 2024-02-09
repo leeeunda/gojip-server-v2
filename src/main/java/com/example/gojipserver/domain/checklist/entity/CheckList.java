4899adeb9d0b1ada5c7d7c3dd2da3df3dfeaf378
strongmhk <kim6562166086@gmail.com>
2024-02-09 21:37:38 +0900

    feat: 체크리스트 엔티티 수정

--- src/main/java/com/example/gojipserver/domain/checklist/entity/CheckList.java
+++ src/main/java/com/example/gojipserver/domain/checklist/entity/CheckList.java
@@ -1,6 +1,6 @@
 package com.example.gojipserver.domain.checklist.entity;
 
-import com.example.gojipserver.domain.address.entity.Address;
+import com.example.gojipserver.domain.roomaddress.entity.RoomAddress;
 import com.example.gojipserver.domain.checklist.entity.bathroomstatus.BathRoomStatus;
 import com.example.gojipserver.domain.checklist.entity.cost.Cost;
 import com.example.gojipserver.domain.checklist.entity.option.InnerOption;
@@ -8,11 +8,16 @@ import com.example.gojipserver.domain.checklist.entity.option.OuterOption;
 import com.example.gojipserver.domain.checklist.entity.roomcondition.RoomCondition;
 import com.example.gojipserver.domain.checklist.entity.roomstatus.RoomStatus;
 import com.example.gojipserver.domain.collection.entity.Collection;
+import com.example.gojipserver.domain.roomimage.entity.RoomImage;
 import com.example.gojipserver.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static jakarta.persistence.FetchType.*;
 
@@ -26,13 +31,20 @@ public class CheckList {
     @Column(name = "check_list_id")
     private Long id;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "room_address_id")
+    private RoomAddress roomAddress;
+
     @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "users_id", nullable = false)
     private User user;
 
-
     @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "collection_id")
     private Collection collection;
+
+    @OneToMany(mappedBy = "checkList", cascade = CascadeType.ALL)
+    private List<RoomImage> roomImages = new ArrayList<>();
 
     @Embedded
     private Cost cost; //비용
@@ -53,7 +65,21 @@ public class CheckList {
     private OuterOption outerOption; //외부 옵션
 
     private String note; //추가 사항
-    
+
+    @Builder
+    public CheckList(RoomAddress roomAddress, User user, Collection collection, List<RoomImage> roomImages, Cost cost, RoomCondition roomCondition, RoomStatus roomStatus, BathRoomStatus bathRoomStatus, InnerOption innerOption, OuterOption outerOption, String note) {
+        this.roomAddress = roomAddress;
+        this.user = user;
+        this.collection = collection;
+        this.roomImages = roomImages;
+        this.cost = cost;
+        this.roomCondition = roomCondition;
+        this.roomStatus = roomStatus;
+        this.bathRoomStatus = bathRoomStatus;
+        this.innerOption = innerOption;
+        this.outerOption = outerOption;
+        this.note = note;
+    }
 
 
 

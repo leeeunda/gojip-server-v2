5e513e5e0d9c765c8b4a17efbe1322ee04fce488
strongmhk <kim6562166086@gmail.com>
2024-02-09 21:31:05 +0900

    feat: 컬렉션 api 코드 구성

--- src/main/java/com/example/gojipserver/domain/collection/entity/Collection.java
+++ src/main/java/com/example/gojipserver/domain/collection/entity/Collection.java
@@ -1,13 +1,21 @@
 package com.example.gojipserver.domain.collection.entity;
 
+import com.example.gojipserver.domain.checklist.entity.CheckList;
 import com.example.gojipserver.domain.user.entity.User;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static jakarta.persistence.FetchType.*;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Collection {
 
     @Id
@@ -19,5 +27,14 @@ public class Collection {
     @JoinColumn(name = "users_id")
     private User user;
 
-    private String name;
+    @OneToMany(mappedBy = "collection")
+    private List<CheckList> checkLists = new ArrayList<>();
+
+    private String collectionName;
+
+    @Builder
+    public Collection(User user, String collectionName) {
+        this.user = user;
+        this.collectionName = collectionName;
+    }
 }

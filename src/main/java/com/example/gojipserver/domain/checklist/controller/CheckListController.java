1598b3c730b1aba3b8d9741e718769d85c5550cd
strongmhk <kim6562166086@gmail.com>
2024-02-12 00:15:45 +0900

    feat: CheckList 컨트롤러, 서비스 계층

--- src/main/java/com/example/gojipserver/domain/checklist/controller/CheckListController.java
+++ src/main/java/com/example/gojipserver/domain/checklist/controller/CheckListController.java
@@ -2,13 +2,10 @@ package com.example.gojipserver.domain.checklist.controller;
 
 import com.example.gojipserver.domain.checklist.dto.CheckListSaveDto;
 import com.example.gojipserver.domain.checklist.service.CheckListService;
-import com.example.gojipserver.global.oauth2.entity.UserPrincipal;
+import com.example.gojipserver.domain.oauth2.entity.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -20,8 +17,9 @@ public class CheckListController {
     private final CheckListService checkListService;
 
     @PostMapping
-    public void saveCheckList(@AuthenticationPrincipal UserPrincipal user, @RequestBody CheckListSaveDto checkListSaveDto) {
-        Long userId = user.getId();
+    public void saveCheckList(@AuthenticationPrincipal UserPrincipal userPrincipal, @RequestBody CheckListSaveDto checkListSaveDto) {
+
 
     }
+
 }

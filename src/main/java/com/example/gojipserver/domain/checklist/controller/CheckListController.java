0284f947cc7dc1b3e61f64b9e08dffebfdadf003
leeeunda <daun0219@gmail.com>
2024-02-11 17:28:52 +0900

    [fix] remove MessageDto

--- src/main/java/com/example/gojipserver/domain/checklist/controller/CheckListController.java
+++ src/main/java/com/example/gojipserver/domain/checklist/controller/CheckListController.java
@@ -5,10 +5,7 @@ import com.example.gojipserver.domain.checklist.service.CheckListService;
 import com.example.gojipserver.global.oauth2.entity.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -23,15 +20,23 @@ public class CheckListController {
     @PostMapping
     public void saveCheckList(@AuthenticationPrincipal UserPrincipal user, @RequestBody CheckListSaveDto checkListSaveDto) {
         Long userId = user.getId();
-
     }
 
-//    // 체크리스트 전체 조회
+    // 컬렉션 등록
+    // 체크리스트 전체 조회
 //    @GetMapping()
-//    public String checkListAllGet(){
-//        return
-//    }
+//    public ResponseEntity<MessageDto> checkListAllGet(){
 //
+//        MessageDto messageDto = new MessageDto();
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+//
+//        messageDto.setStatus(HttpStatus.OK.value());
+//        messageDto.setMessage("전체 조회 성공");
+//        messageDto.setData(CheckList);
+//        return new ResponseEntity<>(messageDto, headers,  )
+//    }
+
 //    // 체크리스트 단일 조회
 //    @GetMapping("/{id}")
 //    public String checkListOneGet(){

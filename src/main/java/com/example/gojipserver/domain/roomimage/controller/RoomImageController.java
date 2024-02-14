ed42851a2568b844a9684d34b7199dce3c8e0e54
leeeunda <daun0219@gmail.com>
2024-02-14 17:34:37 +0900

    [feat] 이미지 업로드 API

--- src/main/java/com/example/gojipserver/domain/roomimage/controller/RoomImageController.java
+++ src/main/java/com/example/gojipserver/domain/roomimage/controller/RoomImageController.java
@@ -7,10 +7,7 @@ import org.springframework.http.MediaType;
 import com.example.gojipserver.domain.roomimage.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;

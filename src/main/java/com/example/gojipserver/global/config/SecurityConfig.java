20d7f96db6d110071873bb1d320f986713028841
SeoYoung <seoyoung7623@naver.com>
2024-02-21 01:15:32 +0900

    feat: login/kakao 접근허용

--- src/main/java/com/example/gojipserver/global/config/SecurityConfig.java
+++ src/main/java/com/example/gojipserver/global/config/SecurityConfig.java
@@ -1,8 +1,6 @@
 package com.example.gojipserver.global.config;
 
-import com.example.gojipserver.domain.oauth2.service.CustomOAuth2UserService;
 import com.example.gojipserver.domain.oauth2.service.CustomUserDetailsService;
-import com.example.gojipserver.global.config.security.handler.OAuth2AuthenticationSuccessHandler;
 import com.example.gojipserver.global.config.security.jwt.JwtAuthenticationFilter;
 import com.example.gojipserver.global.config.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
@@ -20,12 +18,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
     private static final String[] AUTH_WHITELIST = {
             "/oauth2/**", "/login", "/swagger-ui/**", "/api-docs", "/swagger-ui-custom.html",
-            "/v3/api-docs/**", "/api-docs/**", "/swagger-ui.html"
+            "/v3/api-docs/**", "/api-docs/**", "/swagger-ui.html","/login/kakao"
     }; // 인증 필터를 거치지 않는 경로
 
-    private final CustomOAuth2UserService customOAuth2Service;
+
     private final CustomUserDetailsService customUserDetailsService;
-    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
     private final JwtTokenProvider jwtTokenProvider;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {

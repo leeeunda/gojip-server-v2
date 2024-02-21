af098f7f3256b7519080842fe7b11f30ccf4e881
strongmhk <kim6562166086@gmail.com>
2024-02-21 17:01:39 +0900

    refactor: dto 기본 생성자 접근제한자 변경

--- src/main/java/com/example/gojipserver/domain/collection/dto/CollectionSaveDto.java
+++ src/main/java/com/example/gojipserver/domain/collection/dto/CollectionSaveDto.java
@@ -3,13 +3,14 @@ package com.example.gojipserver.domain.collection.dto;
 import com.example.gojipserver.domain.collection.entity.Collection;
 import com.example.gojipserver.domain.user.entity.User;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CollectionSaveDto {
 
     @NotBlank(message = "컬렉션 이름은 필수 입력값입니다.")

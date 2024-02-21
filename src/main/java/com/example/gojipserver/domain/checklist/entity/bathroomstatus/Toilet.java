55311528cdf5c71610dbf4f2e80c23d2e364bac0
strongmhk <kim6562166086@gmail.com>
2024-02-21 16:32:40 +0900

    feat: enum 역직렬화 메서드 구현

--- src/main/java/com/example/gojipserver/domain/checklist/entity/bathroomstatus/Toilet.java
+++ src/main/java/com/example/gojipserver/domain/checklist/entity/bathroomstatus/Toilet.java
@@ -1,6 +1,30 @@
 package com.example.gojipserver.domain.checklist.entity.bathroomstatus;
 
-public enum Toilet {
+import com.example.gojipserver.domain.checklist.entity.roomstatus.Boiler;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
-    GOOD, BAD
+@Getter
+@Slf4j
+public enum Toilet {
+    GOOD("GOOD"),
+    BAD("BAD");
+
+    String value;
+
+    Toilet(String value) {
+        this.value = value;
+    }
+
+    @JsonCreator // Json -> Object, 역직렬화 수행하는 메서드
+    public static Toilet from(String param) {
+        for (Toilet toilet : Toilet.values()) {
+            if (toilet.getValue().equals(param)) {
+                return toilet;
+            }
+        }
+        log.debug("Toilet.from() exception occur param: {}", param);
+        return null;
+    }
 }

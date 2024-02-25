2cc0e8c5a9700b3e89c5773d27ada62dcc2d3472
strongmhk <kim6562166086@gmail.com>
2024-02-25 22:16:50 +0900

    remove: 필요없는 import 제거

--- src/main/java/com/example/gojipserver/domain/checklist/service/CheckListService.java
+++ src/main/java/com/example/gojipserver/domain/checklist/service/CheckListService.java
@@ -15,7 +15,6 @@ import com.example.gojipserver.domain.roomimage.repository.RoomImageRepository;
 import com.example.gojipserver.domain.user.entity.User;
 import com.example.gojipserver.domain.user.repository.UserRepository;
 import com.example.gojipserver.global.exception.NotOwnerException;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;

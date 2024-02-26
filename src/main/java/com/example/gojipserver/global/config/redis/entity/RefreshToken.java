3b0c7a2181afd98682d75e0cfaf8bfcd847a55ea
SeoYoung <seoyoung7623@naver.com>
2024-02-27 01:07:58 +0900

    style: refreshToken entity 정리

--- src/main/java/com/example/gojipserver/global/config/redis/entity/RefreshToken.java
+++ src/main/java/com/example/gojipserver/global/config/redis/entity/RefreshToken.java
@@ -1,19 +1,15 @@
 package com.example.gojipserver.global.config.redis.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 @Getter
 @RedisHash(value = "refreshToken", timeToLive = 14440)
+@AllArgsConstructor
 public class RefreshToken {
-
     @Id
     private String refreshToken;
     private String email;
-
-    public RefreshToken(String refreshToken, String email) {
-        this.refreshToken = refreshToken;
-        this.email = email;
-    }
 }

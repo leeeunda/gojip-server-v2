93fd09e131c3049c042163651d5d4a7c42495a93
SeoYoung <seoyoung7623@naver.com>
2024-03-09 17:14:33 +0900

    refactor: kakao 로그인 리팩토링

--- src/main/java/com/example/gojipserver/domain/oauth2/controller/KakaoController.java
+++ src/main/java/com/example/gojipserver/domain/oauth2/controller/KakaoController.java
@@ -1,79 +1,28 @@
 package com.example.gojipserver.domain.oauth2.controller;
 
-import com.example.gojipserver.domain.oauth2.dto.UserDto.*;
-import com.example.gojipserver.domain.oauth2.entity.UserPrincipal;
+import com.example.gojipserver.domain.oauth2.dto.UserRequestDto.CreateAccessTokenRequest;
 import com.example.gojipserver.domain.oauth2.service.KakaoService;
-import com.example.gojipserver.domain.oauth2.service.OAuthService;
-import com.example.gojipserver.global.config.jwt.JwtTokenProvider;
 import com.example.gojipserver.global.response.ApiResponse;
-import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.example.gojipserver.domain.oauth2.dto.UserResponseDto.*;
 
 @RestController
-@RequestMapping
 @RequiredArgsConstructor
-@Slf4j
-@Tag(name = "Kakao", description = "카카오 로그인 API")
+@Tag(name = "Kakao", description = "카카오 로그인 관련 API")
 public class KakaoController {
     private final KakaoService kakaoService;
-    private final OAuthService oauthService;
-    private final JwtTokenProvider jwtTokenProvider;
-
-    // 카카오 로그인
-    @PostMapping("/login/kakao")
-    @Operation(summary = "카카오 로그인", description = "카카오 로그인 api")
-    @Parameter(name = "token", description = "카카오에서 받은 AccessToken을 헤더에 넣습니다.")
-    public ApiResponse<UserInfoDto> login(@RequestHeader String token,HttpServletResponse response) {
-        UserInfoDto userInfo = kakaoService.getUserProfileByToken(token);
-        String refreshToken = oauthService.getRefreshToken(userInfo.getEmail());
-        String accessToken = oauthService.getAccessToken(userInfo.getEmail(), refreshToken);
-        userInfo.setToken(accessToken,refreshToken);
-        jwtTokenProvider.sendAccessAndRefreshToken(response,accessToken,refreshToken);
-        return ApiResponse.createSuccess(userInfo);
+    @PostMapping("/newToken")
+    @Operation(summary = "새로운 토큰 발급", description = "리프레시 토큰을 이용하여 새로운 액세스 토큰을 발급합니다.")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "리프레시 토큰을 담은 DTO")
+    public ApiResponse<CreatedAccessTokenResponse >  createNewAccessToken(@RequestBody CreateAccessTokenRequest createAccessTokenRequest) {
+        CreatedAccessTokenResponse newAccessToken = kakaoService.createNewAccessToken(createAccessTokenRequest.getRefreshToken());
+        return ApiResponse.createSuccess(newAccessToken, "새로운 액세스 토큰 발급 성공");
     }
 
-    // 유저 테스트
-    @Hidden
-    @GetMapping("/user-details")
-    public ResponseEntity<UserDetails> getUserDetails(@AuthenticationPrincipal UserPrincipal userPrincipal) {
-        log.info("principal : {}", userPrincipal);
-        return ResponseEntity.ok(userPrincipal);
-    }
-
-    // 카카오 로그아웃
-    @Operation(summary = "카카오 로그아웃", description = "카카오 로그아웃 api")
-    @Parameter(name = "Authorization", description = "Bearer + accessToken을 헤더에 넣습니다.")
-    @Parameter(name = "Authorization-refresh", description = "Bearer + refreshToken을 헤더에 넣습니다.")
-    @PostMapping("/logout/kakao")
-    public ApiResponse<String> logout(HttpServletRequest request) {
-        String accessToken = jwtTokenProvider.getJwtFromRequest(request, "Authorization");
-        String refreshToken = jwtTokenProvider.getJwtFromRequest(request, "Authorization-refresh");
-        oauthService.logout(accessToken,refreshToken);
-        return ApiResponse.createSuccess("로그아웃 성공");
-    }
-
-    // 액세스토큰 갱신
-    @Operation(summary = "AccessToken 갱신", description = "RefreshToken을 이용하여 AccessToken을 갱신합니다.")
-    @Parameter(name = "Authorization-refresh", description = "Bearer + refreshToken을 헤더에 넣습니다.")
-    @GetMapping("/refresh")
-    public ApiResponse<?> refresh(HttpServletRequest request,HttpServletResponse response) {
-        String refreshToken = jwtTokenProvider.getJwtFromRequest(request,"Authorization-refresh");
-        try{
-            jwtTokenProvider.validateToken(request,refreshToken);
-            AccessTokenDto accessTokenDto = oauthService.updateAccessToken(refreshToken);
-            return ApiResponse.createSuccess(accessTokenDto);
-        }catch (Exception e) {
-            return ApiResponse.create400Error(e.getMessage());
-        }
-    }
 }
--- src/main/java/com/example/gojipserver/domain/oauth2/service/KakaoService.java
+++ src/main/java/com/example/gojipserver/domain/oauth2/service/KakaoService.java
@@ -1,69 +1,29 @@
 package com.example.gojipserver.domain.oauth2.service;
 
-import com.example.gojipserver.domain.oauth2.dto.KakaoUserInfoDto.*;
-import com.example.gojipserver.domain.oauth2.dto.UserDto;
-import com.example.gojipserver.domain.oauth2.dto.UserDto.*;
-import com.example.gojipserver.domain.user.entity.Role;
+import com.example.gojipserver.domain.oauth2.dto.UserResponseDto;
 import com.example.gojipserver.domain.user.entity.User;
 import com.example.gojipserver.domain.user.repository.UserRepository;
+import com.example.gojipserver.global.config.jwt.JwtTokenProvider;
 import com.example.gojipserver.global.config.redis.repository.RefreshTokenRepository;
 import lombok.RequiredArgsConstructor;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import static com.example.gojipserver.domain.oauth2.dto.UserResponseDto.*;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
-public class KakaoService{
-    private final UserRepository userRepository;
+public class KakaoService {
+    private final JwtTokenProvider jwtTokenProvider;
     private final RefreshTokenRepository refreshTokenRepository;
-    private final WebClient webClient;
+    private final UserRepository userRepository;
 
-    public UserInfoDto getUserProfileByToken(String accessToken){
-        KakaoUserInfoResponse kakaoInfoDto = getUserAttributesByToken(accessToken);
-        User newUser = User.builder()
-                .id(kakaoInfoDto.getId())
-                .email(kakaoInfoDto.getKakao_account().getEmail())
-                .nickname(kakaoInfoDto.getProperties().getNickname())
-                .role(Role.USER)
-                .build();
-        User findUser = userRepository.findByEmail(newUser.getEmail()).orElse(null);
-        if(findUser != null) {
-            findUser.update(kakaoInfoDto.getKakao_account().getEmail(), kakaoInfoDto.getProperties().getNickname());
-        } else {
-            extracted(newUser);
+    public CreatedAccessTokenResponse createNewAccessToken(String refreshToken) {
+        if (!jwtTokenProvider.validateToken(refreshToken)) {
+            throw new IllegalArgumentException("유효하지 않은 토큰입니다.");
         }
-        UserInfoDto userDto = UserInfoDto.builder()
-                .id(newUser.getId())
-                .email(newUser.getEmail())
-                .nickname(newUser.getNickname())
-                .role(newUser.getRole())
-                .build();
-        return userDto;
-    }
 
-    public void extracted(User newUser) {
-        userRepository.save(newUser);
-    }
+        Long userId = refreshTokenRepository.findById(refreshToken).orElseThrow(() -> new IllegalArgumentException("리프레시토큰이 존재하지않습니다.")).getUserId();
 
-    public KakaoUserInfoResponse getUserAttributesByToken(String accessToken){
-        Flux<KakaoUserInfoResponse> response = webClient.get()
-                .uri("https://kapi.kakao.com/v2/user/me")
-                .header("Authorization", "Bearer " + accessToken)
-                .retrieve()
-                .bodyToFlux(KakaoUserInfoResponse.class);
-        return response.blockFirst();
+        return new CreatedAccessTokenResponse(jwtTokenProvider.createAccessToken(userId));
     }
 }

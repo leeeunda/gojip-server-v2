2fd4c44be2cd5deea1b6dc97ff7fc7ad10b16138
leeeunda <daun0219@gmail.com>
2024-03-15 10:52:28 +0900

    add Scheduling anotation

--- src/main/java/com/example/gojipserver/GojipServerApplication.java
+++ src/main/java/com/example/gojipserver/GojipServerApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
+@EnableScheduling
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class GojipServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GojipServerApplication.class, args);
     }
-
 }

7874eebf323fd31d29c22a4100ccc0b1c9ff1d10
strongmhk <kim6562166086@gmail.com>
2024-03-16 21:59:45 +0900

    fix: 공통 응답 클래스에 errors 추가
    다중 필드 검증을 위해 errors map 추가

--- src/main/java/com/example/gojipserver/global/response/ApiResponse.java
+++ src/main/java/com/example/gojipserver/global/response/ApiResponse.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Map;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ApiResponse<T> {
@@ -14,47 +16,51 @@ public class ApiResponse<T> {
     private StatusEnum status;
     private T data;
     private String message;
+    private Map<String, String> errors; // 파라미터 여러 개에서 예외가 발생했을 경우
 
-    public static <T> ApiResponse<T> createSuccess(T data) {
-        return new ApiResponse<>(true, StatusEnum.OK, data, null);
+    public static <T> ApiResponse<T> responseSuccess(T data) {
+        return new ApiResponse<>(true, StatusEnum.OK, data, null, null);
     }
-    public static <T> ApiResponse<T> createSuccess(T data, String message) {
-        return new ApiResponse<>(true, StatusEnum.OK, data, message);
-    }
-
-    public static <T> ApiResponse<T> createSuccess(T data, StatusEnum status, String message) {
-        return new ApiResponse<>(true, status, data, message);
+    public static <T> ApiResponse<T> responseSuccess(T data, String message) {
+        return new ApiResponse<>(true, StatusEnum.OK, data, message, null);
     }
 
-    public static <T> ApiResponse<T> create201Success(T data, String message) {
-        return new ApiResponse<>(true, StatusEnum.CREATED, data, message);
+    public static <T> ApiResponse<T> responseSuccess(T data, StatusEnum status, String message) {
+        return new ApiResponse<>(true, status, data, message, null);
+    }
+
+    public static <T> ApiResponse<T> response201Success(T data, String message) {
+        return new ApiResponse<>(true, StatusEnum.CREATED, data, message, null);
     }
 
 
 
-    public static ApiResponse<?> createSuccessWithNoContent() {
-        return new ApiResponse<>(true, StatusEnum.OK, null, null);
+    public static ApiResponse<?> responseSuccessWithNoContent() {
+        return new ApiResponse<>(true, StatusEnum.OK, null, null, null);
     }
 
 
     // 예외 발생으로 API 호출 실패시 반환
-    public static ApiResponse<?> create400Error(String message) {
-        return new ApiResponse<>(false, StatusEnum.BAD_REQUEST, null, message);
+    public static ApiResponse<?> response400Error(String message, Map<String, String> errors) {
+        return new ApiResponse<>(false, StatusEnum.BAD_REQUEST, null, message, errors);
     }
 
-    public static ApiResponse<?> create403Error(String message) {
-        return new ApiResponse<>(false, StatusEnum.FORBIDDEN, null, message);
+    public static ApiResponse<?> response403Error(String message, Map<String, String> errors) {
+        return new ApiResponse<>(false, StatusEnum.FORBIDDEN, null, message, errors);
     }
 
-    public static ApiResponse<?> create404Error(String message) {
-        return new ApiResponse<>(false, StatusEnum.NOT_FOUND, null, message);
+    public static ApiResponse<?> response404Error(String message) {
+        return new ApiResponse<>(false, StatusEnum.NOT_FOUND, null, message, null);
     }
 
     @Builder
-    private ApiResponse(boolean isSuccess, StatusEnum status, T data, String message) {
+    private ApiResponse(boolean isSuccess, StatusEnum status, T data, String message, Map<String, String> errors) {
         this.isSuccess = isSuccess;
         this.status = status;
         this.data = data;
         this.message = message;
+        this.errors = errors;
     }
+
+
 }
